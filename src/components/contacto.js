c641201a72e3659ce92fa43059082d87a10a6b2c
Andres <andresllontop01@gmail.com>
2020-06-17 00:08:30 -0500

    Se agregó las librerias de boostrap y font awesone, pero no muestra el icono

--- src/components/contacto.js
+++ src/components/contacto.js
@@ -3,9 +3,9 @@ import React, { Component } from "react";
 class Contacto extends Component {
   render() {
     return (
-      <div className="caja">
-        <h1>{this.props.nombre}</h1>
-        <>
+      <div className="caja">        
+        <h1><i className="fas fa-user-circle"></i>{this.props.nombre}</h1>
+        <>        
           <p>Teléfono: {this.props.telef}</p>
           <p>Dirección: {this.props.direc}</p>
         </>
